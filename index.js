11e9d4a7b87f2a22750d0ccd33c996a51ffec689
Brian Vanderbusch <chief@hackerhappyhour.com>
2020-08-08 13:36:36 -0500

    input testing

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ try {
   // get tag-pattern-matcher
   const pattern = core.getInput('pattern')
   const tag = core.getInput('tag_name')
-  console.log(`Parsing ${tag_name} with ${pattern} tag for this run`)
+  console.log(`Parsing with ${pattern} tag for this run`)
+  console.log('tag', tag)
 
   // this will be a function to parse the input against the event payload
   // to produce a refined tag
