62a8e9dba8a85817541922cb3484fda5c7705e8a
Brian Vanderbusch <chief@hackerhappyhour.com>
2020-08-09 16:09:33 -0500

    cleanup parser.spec.js

--- src/parser.spec.js
+++ src/parser.spec.js
@@ -4,28 +4,28 @@ const {
   replacers 
 } = require('./parser')
 
-describe('if no delimiters found', ()=>{
-  test('it accepts "latest"', () => {
-    const tag = parseTag('latest', '1.0.0')
-    expect(tag).toMatchObject({tag:'latest'})
-  })
+const strategies = [
+  ['%X.Y.Z%-camera', '1.0.0-rc1', '1.0.0-rc1-camera'],
+  ['%X.Y.Z%-camera', '1.0.0-rc.1', '1.0.0-rc.1-camera']
+]
 
-  test.skip('it returns an error if any un-parseable alpha chars exist', () => {
-    let strategy = '%X.1.Z%-foobar'
-    let release = '1.0.0'
-    const {invalidTag} = errors
-    let {tag, error} = parseTag(strategy, release)
-    expect(error).toMatchObject(invalidTag)
-  })
+test('it accepts "latest"', () => {
+  const tag = parseTag('latest', '1.0.0')
+  expect(tag).toMatchObject({tag:'latest'})
 })
 
-describe('if delimiters found', () => {
-  const strategies = [
-    ['%X.Y.Z%-camera', '1.0.0-rc1', '1.0.0rc1-camera'],
-    ['%X.Y.Z%-camera', '1.0.0-rc.1', '1.0.0-rc.1-camera']
-  ]
-  test.each(strategies)('.parseTag("%s", "%s") expects %s', (strategy, release, expected) => {
-    let {tag} = parseTag(strategy, release)
-    expect(tag).toMatch(expected)
-  })
+test.each(strategies)(
+  '.parseTag("%s", "%s") expects %s', 
+  (strategy, release, expected) => {
+  let {tag} = parseTag(strategy, release)
+  expect(tag).toMatch(expected)
 })
+
+test.skip('it returns an error if any un-parseable alpha chars exist', () => {
+  let strategy = '%X.1.Z%-foobar'
+  let release = '1.0.0'
+  const {invalidTag} = errors
+  let {tag, error} = parseTag(strategy, release)
+  expect(error).toMatchObject(invalidTag)
+})
+
