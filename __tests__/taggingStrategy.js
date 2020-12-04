23483aee5f3ac00e20e66698ca1c6204792be43c
Brian Vanderbusch <chief@hackerhappyhour.com>
2020-12-04 11:21:47 -0600

    fix testing call causing false positives

--- __tests__/taggingStrategy.js
+++ __tests__/taggingStrategy.js
@@ -3,6 +3,6 @@ const {scenarios} = require('./__data__/taggingStrategy')
 
 describe.each(scenarios)('produce tags %s given %o', (expected, given) => {
   test('it produces a csv list', () => {
-    expect(taggingStrategy(given)).toMatch(expected)
+    expect(taggingStrategy(given)).toEqual(expected)
   })
 })
